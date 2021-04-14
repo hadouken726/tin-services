1524b978a284d08cdf6a6f5b3f41043b785c835b
marcelonicastro <nicastro_carvalho@hotmail.com>
2021-04-14 12:06:02 -0300

    feature: dashboard-form-create-posts

---
+++ src/components/DashBoard/DashBoardFormCreatePost/styled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const DivContainer = styled.div`
+  
+`;
+
+export const Titulo = styled.div`
+  
+`;
+
+export const ComboBox = styled.div`
+  
+`;
+
+export const SubTitulo = styled.div`
+  
+`;
+
+export const Description = styled.div`
+  
+`;
