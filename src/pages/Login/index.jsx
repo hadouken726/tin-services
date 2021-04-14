d6a56f43f6f10b81887f42a37afea658c63175a5
gustavodm <gusttavomota@gmail.com>
2021-04-14 12:34:01 -0300

    feat: add useAuth hook

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,18 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
-import jwt_decode from "jwt-decode";
-
 import Glass from "../../components/Glass";
 import Header from "../../components/Header";
 import { Link } from "react-router-dom";
 import { FiMail, FiLock, FiLogIn, FiAlertTriangle } from "react-icons/fi";
-
 import loginImg from "../../assets/login.svg";
-
 import api from "../../services/api";
-
+import {useHistory} from "react-router-dom"
 import {
   Container,
   Content,
@@ -22,74 +18,68 @@ import {
   IconBox,
   Button,
 } from "./styles";
+import {useAuth} from "../../providers/Auth"
 
 const Login = () => {
-  const schema = yup.object().shape({
-    email: yup.string("Somente texto").email().required("Campo obrigatório"),
-    password: yup
-      .string()
-      .min(5, "Mínimo de 8 dígitos")
-      .required("Campo obrigatório"),
-  });
+    const schema = yup.object().shape({
+        email: yup.string("Somente texto").email().required("Campo obrigatório"),
+        password: yup
+            .string()
+            .min(5, "Mínimo de 8 dígitos")
+            .required("Campo obrigatório"),
+    });
+    const {
+        register,
+        handleSubmit,
+        formState: {errors},
+    } = useForm({
+        resolver: yupResolver(schema),
+    });
+    const {signIn} = useAuth()
+    const history = useHistory()
+    const [fetchError, setFetchError] = useState(false);
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
-  });
+    return (
+        <Container>
+            <Glass size={90}>
+                <Header/>
+                <Content>
+                    <img src={loginImg} alt="Login" draggable="false"/>
+                    <Form onSubmit={handleSubmit(userData => signIn(userData, setFetchError, history))}>
+                        <p>Faça seu login</p>
+                        <InputBox>
+                            <IconBox>
+                                {errors.email ? <FiAlertTriangle color="red"/> : <FiMail/>}
+                            </IconBox>
+                            <Input
+                                type="email"
+                                {...register("email")}
+                                placeholder={errors.email ? errors.email.message : "Seu e-mail"}
+                            />
+                        </InputBox>
 
-  const handleForm = async (data) => {
-    const response = await api.post("login", data);
-    const token = response.data.accessToken;
-    const { sub } = jwt_decode(token);
+                        <InputBox>
+                            <IconBox>
+                                {errors.email ? <FiAlertTriangle color="red"/> : <FiLock/>}
+                            </IconBox>
+                            <Input
+                                type="password"
+                                {...register("password")}
+                                placeholder={errors.email ? errors.email.message : "Sua senha"}
+                            />
+                        </InputBox>
 
-    localStorage.setItem("token", JSON.stringify(token));
-    localStorage.setItem("id", JSON.stringify(sub));
-  };
+                        <Button type="submit">Login</Button>
 
-  return (
-    <Container>
-      <Glass size={90}>
-        <Header />
-        <Content>
-          <img src={loginImg} alt="Login" draggable="false" />
-          <Form onSubmit={handleSubmit(handleForm)}>
-            <p>Faça seu login</p>
-            <InputBox>
-              <IconBox>
-                {errors.email ? <FiAlertTriangle color="red" /> : <FiMail />}
-              </IconBox>
-              <Input
-                type="email"
-                {...register("email")}
-                placeholder={errors.email ? errors.email.message : "Seu e-mail"}
-              />
-            </InputBox>
-
-            <InputBox>
-              <IconBox>
-                {errors.email ? <FiAlertTriangle color="red" /> : <FiLock />}
-              </IconBox>
-              <Input
-                type="password"
-                {...register("password")}
-                placeholder={errors.email ? errors.email.message : "Sua senha"}
-              />
-            </InputBox>
-
-            <Button type="submit">Login</Button>
-
-            <Link className="callLink" to="/register">
-              <FiLogIn />
-              Não tem registro? Vem por aqui!
-            </Link>
-          </Form>
-        </Content>
-      </Glass>
-    </Container>
-  );
+                        <Link className="callLink" to="/register">
+                            <FiLogIn/>
+                            Não tem registro? Vem por aqui!
+                        </Link>
+                    </Form>
+                </Content>
+            </Glass>
+        </Container>
+    );
 };
 
 export default Login;
--- src/providers/Auth/index.jsx
+++ src/providers/Auth/index.jsx
@@ -1,8 +1,23 @@
 import {useContext,createContext,useState} from "react"
+import api from "../../services/api"
+import jwt_decode from "jwt-decode";
 const AuthContext = createContext()
 export const AuthProvider = ({children}) => {
     const token = localStorage.getItem("token") || ""
     const [auth, setAuth] = useState({})
-    return <AuthContext.Provider value={{auth, setAuth}} >{children}</AuthContext.Provider>
+    const signIn = (userData, setError, history) => {
+        api
+            .post("/login", userData)
+            .then((response) => {
+                const {sub} = jwt_decode(response.data.accessToken)
+                localStorage.setItem("token", JSON.stringify(response.data.accessToken));
+                localStorage.setItem("userId", JSON.stringify(sub));
+                setAuth({...auth, "token":response.data.accessToken, "userId":sub});
+                history.push("/dashboard");
+            })
+            .catch((err) => setError(true));
+    }
+
+    return <AuthContext.Provider value={{auth, setAuth, signIn}} >{children}</AuthContext.Provider>
 }
 export const useAuth = () => useContext(AuthContext);
