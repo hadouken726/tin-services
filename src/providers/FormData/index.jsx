df3a3778618b2d19764837c0da3f9cba4b4c53c7
Gustavo  Mota <gusttavomota@gmail.com>
2021-04-16 14:13:15 +0000

    Cleared src/providers/FormData/index.js

--- src/providers/FormData/index.jsx
+++ src/providers/FormData/index.jsx
@@ -1,14 +1,1 @@
-import { createContext, useContext, useState } from "react";
-const FormDataContext = createContext();
-export const FormDataProvider = ({ children }) => {
-  const [formData, setFormData] = useState({});
-  return (
-    <FormDataContext.Provider value={{ formData, setFormData }}>
-      {children}
-    </FormDataContext.Provider>
-  );
-};
-export const useFormData = () => {
-  const { formData, setFormData } = useContext(FormDataContext);
-  return { formData, setFormData };
-};
+
