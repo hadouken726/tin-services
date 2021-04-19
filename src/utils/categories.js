b864c95c64def76ec3cddc9b4f291f0e388f9caf
gustavodm <gusttavomota@gmail.com>
2021-04-19 09:26:50 -0300

    feat: add category eletricista

--- src/utils/categories.js
+++ src/utils/categories.js
@@ -12,4 +12,10 @@ export const categories = [
     desc:
       "Isso inclui tanto a área interna quanto a área externa; -Varrer, limpar e passar pano no chão; -Realizar a limpeza de vidros e vitrais, bem como sua manutenção; ... Isso inclui a limpeza da parte externa de vidros que estão em pavimentos mais altos, como sobrados ou prédios.",
   },
+    {
+        "id": 3,
+        "name": "Eletricista",
+        "desc": "O Eletricista é o profissional que entende da rede elétrica de residências e de empresas. Ele realiza manutenção preventiva e corretiva, faz instalação de quadros de distribuição de força e analisa o consumo de energia."
+    }
+
 ];
